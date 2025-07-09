60e6e969b96ee2c2ab9468977cf84e007988f056
Nikola Rusimovic <nikola.rusimovic@hyperoptic.com>
2025-07-09 11:44:04 +0200

    create login form

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,50 +1,66 @@
 import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {
-  IonButton,
-  IonCol,
-  IonContent,
-  IonGrid,
-  IonHeader,
-  IonInput,
-  IonItem,
-  IonList,
-  IonLoading,
-  IonRow,
-  IonTitle,
-  IonToolbar
+    IonButton,
+    IonCol,
+    IonContent,
+    IonGrid,
+    IonHeader,
+    IonInput,
+    IonItem,
+    IonList,
+    IonLoading,
+    IonRow,
+    IonText,
+    IonTitle,
+    IonToolbar
 } from '@ionic/angular/standalone';
 import {AuthService} from "./auth.service";
 import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.page.html',
-  styleUrls: ['./auth.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonLoading, IonGrid, IonRow, IonCol, IonList, IonItem, IonInput]
+    selector: 'app-auth',
+    templateUrl: './auth.page.html',
+    styleUrls: ['./auth.page.scss'],
+    standalone: true,
+    imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, IonButton, IonLoading, IonGrid, IonRow, IonCol, IonList, IonItem, IonInput, FormsModule, ReactiveFormsModule, IonText]
 })
 export class AuthPage implements OnInit {
 
-  loading: boolean = false;
+    loading: boolean = false;
+    authForm!: FormGroup;
 
-  constructor(private authService: AuthService,
-              private router: Router) { }
+    constructor(private authService: AuthService,
+                private router: Router,
+                private formBuilder: FormBuilder) {
+        this.authForm = this.formBuilder.group({
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]]
+        });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  onLogin() {
-    this.loading = true;
-    this.authService.login();
-    setTimeout( () => {
-          this.loading = false;
-        }
-        , 1000)
-  }
+    get email() {
+      return this.authForm.get('email');
+    }
 
-  openHomePage() {
-    this.router.navigate(['/', 'places', 'tabs', 'discover']);
-  }
+    get password() {
+      return this.authForm.get('password');
+    }
+
+    onLogin() {
+        this.loading = true;
+        this.authService.login();
+        setTimeout(() => {
+                this.loading = false;
+            }
+            , 1000)
+    }
+
+    openHomePage() {
+        this.router.navigate(['/', 'places', 'tabs', 'discover']);
+    }
 }
